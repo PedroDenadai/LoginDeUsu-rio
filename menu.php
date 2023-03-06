c3527b1256337d9a26d649f12315cf73385a3f85
PedroDenadai <93556114+PedroDenadai@users.noreply.github.com>
2023-03-06 07:49:07 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    <p>
+        <a href="cadastro.php"><button class="botao_formulario">Cadastro de Usuário</button></a>
+    </p>
+    <p>
+        <a href="admissao.php"><button class="botao_formulario">Cadastro de Funcionario</button></a>
+    </p>
+    <p>
+        <a href="logout.php"><button  class="botao_logout">Sair</button></a>
+    </p>
+</body>
+</html>
